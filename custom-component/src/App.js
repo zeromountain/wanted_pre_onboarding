982121b85de2ab7710a4e923624002c07c92b062
zeromountain <sonyeongsan@gmail.com>
2022-01-27 02:05:45 +0900

    :sparkles: feature: Click To Edit

--- custom-component/src/App.js
+++ custom-component/src/App.js
@@ -1,8 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
 
+import Toggle from './components/Toggle';
+import Modal from './components/Modal';
+import Tab from './components/Tab';
+import Tag from './components/Tag';
+import Autocomplete from './components/AutoComplete';
+import ClickToEdit from './components/ClickToEdit';
+
 function App() {
-  return <h3 class='text-3xl font-bold underline'>Hello tailwindcss</h3>;
+  return (
+    <>
+      <Toggle />
+      <Modal />
+      <Tab />
+      <Tag />
+      <Autocomplete />
+      <ClickToEdit />
+    </>
+  );
 }
 
 export default App;
--- custom-component/src/components/ClickToEdit.js
+++ custom-component/src/components/ClickToEdit.js
@@ -1,7 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const ClickToEdit = () => {
-  return <div></div>;
+  const [user, setUser] = useState({ name: '김코딩', age: 20 });
+
+  const handleInputChange = (e) => {
+    setUser({ ...user, [e.target.name]: e.target.value });
+  };
+
+  return (
+    <div>
+      <h2>Click To Edit</h2>
+      <span>이름</span>
+      <input
+        type='text'
+        name='name'
+        defaultValue={user.name}
+        onBlur={handleInputChange}
+      />
+      <span>나이</span>
+      <input
+        type='text'
+        name='age'
+        defaultValue={user.age}
+        onBlur={handleInputChange}
+      />
+      <span>
+        이름: {user.name}
+        나이: {user.age}
+      </span>
+    </div>
+  );
 };
 
 export default ClickToEdit;
