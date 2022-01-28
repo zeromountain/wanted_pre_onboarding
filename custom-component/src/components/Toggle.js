d9fdacd8227e0f81e21180b268487ec7fb073ad3
zeromountain <sonyeongsan@gmail.com>
2022-01-29 00:23:14 +0900

    :sparkles: feature: add text content about Toggle

--- custom-component/src/components/Toggle.js
+++ custom-component/src/components/Toggle.js
@@ -1,17 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Toggle = () => {
+  const [on, setOn] = useState(false);
+
+  const handleCheckboxClick = () => {
+    setOn((prev) => !prev);
+  };
+
   return (
     <div className='flex flex-col border-2 w-1/2 mx-auto rounded-lg p-2 h-40 mb-10'>
       <h2 className='font-bold'>토글</h2>
-      <div className='flex justify-center items-center my-2'>
+      <div className='flex flex-col justify-center items-center my-2'>
         <label htmlFor='toggle-switch'>
           <input
             type='checkbox'
             id='toggle-switch'
-            className='cursor-pointer h-16 w-32 rounded-full appearance-none bg-gray-500 bg-opacity-50 checked:bg-violet-800 transition duration-200 '
+            className='cursor-pointer h-12 w-24 rounded-full appearance-none bg-gray-500 bg-opacity-50 checked:bg-violet-800 transition duration-200 '
+            onClick={handleCheckboxClick}
           />
         </label>
+        <span>Toggle Switch {on ? 'ON' : 'OFF'}</span>
       </div>
     </div>
   );
